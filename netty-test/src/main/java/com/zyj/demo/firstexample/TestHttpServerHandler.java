38a4be7798dab94464d86e1662a3c0c7738e2412
yongjian.zhou <yongjian.zhou@beebank.com>
2020-10-23 17:49:07 +0800

    netty http

--- netty-test/src/main/java/com/zyj/demo/firstexample/TestHttpServerHandler.java
+++ netty-test/src/main/java/com/zyj/demo/firstexample/TestHttpServerHandler.java
@@ -1,5 +1,6 @@
 package com.zyj.demo.firstexample;
 
+import com.sun.jndi.toolkit.url.Uri;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
@@ -7,13 +8,68 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
-        ctx.writeAndFlush(response);
+        System.out.println(msg.getClass());
+        System.out.println("客户端端口号"+ ctx.channel().remoteAddress());
+        System.out.println("本地"+ ctx.channel().localAddress());
+
+        if (msg instanceof  HttpRequest){
+            HttpRequest httpRequest = (HttpRequest)msg;
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())){
+                System.out.println("请求favicon.ico");
+                return;
+            }
+            ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+            ctx.writeAndFlush(response);
+            ctx.channel().close();
+        }
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(" handler remove");
+        super.handlerRemoved(ctx);
+
     }
 }
