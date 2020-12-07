094b6ca685a20fc1b724142d8b76c7258c2ccfd9
yongjian.zhou <yongjian.zhou@beebank.com>
2020-12-07 17:13:03 +0800

    producer

---
+++ kafka-test/src/main/java/com/zyj/kafka/partitioner/MyPartitioner.java
@@ -0,0 +1,24 @@
+package com.zyj.kafka.partitioner;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+/**
+ * 自定义分区 可参考DefaultPartitioner
+ */
+public class MyPartitioner implements Partitioner {
+
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        return 0;
+    }
+
+    public void close() {
+
+    }
+
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
--- kafka-test/src/main/java/com/zyj/kafka/producer/PrititionerProducer.java
+++ kafka-test/src/main/java/com/zyj/kafka/producer/PrititionerProducer.java
@@ -1,18 +1,22 @@
-package com.zyj.kafka;
+package com.zyj.kafka.producer;
 
+import com.zyj.kafka.partitioner.MyPartitioner;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.Future;
 
-public class MyProducer {
+public class PrititionerProducer {
     public static void main(String[] args) {
         // 1、创建kafka生产者的配置信息
         Properties properties = new Properties();
 
         // 2、指定连接的kafka集群
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"");
+
 
         // 3、ack应答级别
         properties.put(ProducerConfig.ACKS_CONFIG,"all");
@@ -36,9 +40,18 @@ public class MyProducer {
         // 9、创建生产者对象
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
+
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,"com.zyj.kafka.partitioner.MyPartitioner");
         // 10、发送数据
         for (int i = 0; i < 10; i++) {
-            producer.send(new ProducerRecord<String, String>("first", "zyj" + i));
+            Future<RecordMetadata> first = producer.send(new ProducerRecord<String, String>("first", "zyj" + i));
+            try{
+                RecordMetadata recordMetadata = first.get();// 异步变同步，使用场景，保证消息有序性的时候设置一个partition并不能完全保证有序，这时候可以改为同步发送
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
         }
         // 11、关闭资源
         producer.close();
